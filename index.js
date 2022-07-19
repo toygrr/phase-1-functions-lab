4de68ed8b6fb61deba16e63f76284b7ea897b9ca
Tyler <toygrrr@gmail.com>
2022-07-19 15:37:36 -0700

    commit tree 2c35c3628ee6d572c5370f04ef375dc92f262b8f

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-function distanceFromHqInBlocks(blocks){
-    if (blocks > 42){
-        return blocks - 42;
+function distanceFromHqInBlocks(blocks) {
+    console.log(blocks)
+    if(blocks > 42) {
+        return blocks - 42
     } else {
-        return 42 - blocks;
-  }
- }
+        return 42 - blocks
+    }
+}
 
- function distanceFromHqInFeet(distance){
-    return (distanceFromHqInBlocks(distance) * 264)
- }
+function distanceFromHqInFeet(blocks) {
+    return (distanceFromHqInBlocks(blocks) * 264)
+}
 
-
-function distanceTravelledInFeet(start, destination){
-    if (start > destination){
-        return (start - destination) * 264;
+function distanceTravelledInFeet(start, end) {
+    if (start < end) {
+        return (end - start) * 264;
     }
     else {
-        return (destination - start) * 264;
+        return (start - end) * 264;
     }
 }
 
-function calculatesFarePrice(start, destination){
-const distance = distanceTravelledInFeet(start, destination);
+// const distance = distanceTravelledInFeet();
 
-if (distance < 400) {
-    return 0;
-} else if (distance > 400 && distance <= 2000){
-    return .02 * (distance - 400);
-} else if (distance > 2000 && distance < 2500){
-    return 25;
-} else {
-    return 'cannot travel that far'
-}
+function calculatesFarePrice(start, end) {
+    const distance = distanceTravelledInFeet(start, end);
+
+    if (distance <= 400) {
+     return 0;
+ }
+ 
+    else if (distance > 400 && distance <= 2000) {
+     return (distance - 400) * .02;
+ }
+    else if (distance > 2000 && distance <= 2500) {
+     return 25;
+ }
+    else {
+        return 'cannot travel that far'
+       }
 }
